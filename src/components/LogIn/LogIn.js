32f6acbdc04e4b4ab5c0d8e3a73a1a3709023aef
pulok <khandokarmeherab@gmail.com>
2022-10-22 01:07:56 +0600

    log in and sign in done

--- src/components/LogIn/LogIn.js
+++ src/components/LogIn/LogIn.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './LogIn.css'
 const LogIn = () => {
     return (
@@ -7,13 +8,21 @@ const LogIn = () => {
             <form >
                 <div className='form-control'>
                     <label htmlFor='email'>
-                    email
+                    Email
                     </label>
-                    <input type="email" name="email" id=""/>
+                    <input type="email" name="email" required/>
+                    <label htmlFor='email'>
+                   Password
+                    </label>
+                    <input type="password" name="password" required/>
+                    
 
                 </div>
-
+                   <input className='button-submit' type="submit" value="Log In"/>
             </form>
+            <input className='button-submit' type="submit" value="Sign in With Google"/>
+            <p>new to emajhon? <Link to='/signup'>Create New Account</Link></p>
+
             
         </div>
     );
--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,10 +1,34 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import'./SignUp.css'
 const SignUp = () => {
     return (
-        <div>
-            
-        </div>
+        <div className='form-container'>
+        <h2 className='form-title'>Sign In!!</h2>
+        <form >
+            <div className='form-control'>
+                <label htmlFor='email'>
+                name
+                </label>
+                <input type="text" name="name" required/>
+                <label htmlFor='email'>
+                Email
+                </label>
+                <input type="email" name="email" required/>
+                <label htmlFor='email'>
+               Password
+                </label>
+                <input type="password" name="password" required/>
+                
+
+            </div>
+               <input className='button-submit' type="submit" value="Sign In"/>
+        </form>
+        <input className='button-submit' type="submit" value="Sign in With Google"/>
+        <p>Already have an account? <Link to='/login'>Log In</Link></p>
+
+        
+    </div>
     );
 };
 
